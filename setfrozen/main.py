5041faadba0f374104d9e33d420710b05a5a8586
AsokTamang <ashoktmg2058@gmail.com>
2026-01-09 23:50:07 -0500

    data extraction

--- setfrozen/main.py
+++ setfrozen/main.py
@@ -20,3 +20,31 @@ prices_after_discount = [(price-0.15*price) if price > 100 else price for price 
 
 print(prices_after_discount)
 print(price_earned_dict)  #creating a dictionary based on comprehension method
+
+
+transactions = [
+    {'customer_name':'Asok','amount':250,'date':'2002-03-24'},
+    {'customer_name':'Asok','amount':750,'date':'2002-03-24'},
+    {'customer_name':'Sita','amount':400,'date':'2001-03-24'},
+    {'customer_name':'Gita','amount':650,'date':'2000-03-24'}
+]
+
+unique_names = {transaction['customer_name'] for transaction in transactions}
+print(set(unique_names))
+
+products = [
+    {"product_name": "Laptop", "category": "Electronics", "price": 1200},
+    {"product_name": "Jeans", "category": "Apparel", "price": 40},
+    {"product_name": "Coffee Maker", "category": "Home Appliances", "price": 80},
+    {"product_name": "Smartphone", "category": "Electronics", "price": 999},
+    {"product_name": "Jacket", "category": "Apparel", "price": 60},
+    {"product_name": "Blender", "category": "Home Appliances", "price": 150},
+    {"product_name": "Book", "category": "Literature", "price": 15}
+]
+
+# extracting the data based on the categories
+
+electronics = [product['product_name'] for product in products if product['category']=='Electronics']
+Apparel = [product['product_name'] for product in products if product['category']=='Apparel']
+Home_Appliances = [product['product_name'] for product in products if product['category']=='Home Appliances']
+Literature = [product['product_name'] for product in products if product['category']=='Literature']
