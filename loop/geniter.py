51aadd590a6126a7eff688f6a882f967deb1afe3
AsokTamang <ashoktmg2058@gmail.com>
2026-01-10 20:20:20 -0500

    fibonacci series using generator

--- loop/geniter.py
+++ loop/geniter.py
@@ -13,6 +13,16 @@ c=Controller()
 for data in c:
     print(data)
 
-#so the main advantage of using generator is that we can read or unpack the objects from large volume of data without facing the slowness and memory full outburst with the help
-#of yield which only returns the required data at the current iteration without stopping the function
+#so the main advantage of using generator is that we can read or iterate over the objects from large volume of data without facing the slowness and memory full outburst with the help
+#of yield which only returns the required data at the current iteration without closing the function, as the function stops temporarily and again resumes from the current state
+
+#fibonacci series using generators
+def gen_fib():
+    a,b= 0,1
+    while True:
+        yield a
+        a,b = b,a+b
+fb=gen_fib()
+for _ in range(10):
+    print(next(fb))  #only by using next we can print the yieled value from the function otherwise we will only get the generator object
 
